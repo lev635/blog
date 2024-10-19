19550580954ef267d3a828247ddab2438e5a33b3
lev635 <ryux635@gmail.com>
2024-10-19 17:29:23 +0900

    更新日時追加、home変更、シンタックスハイライト

--- article/js/script.js
+++ article/js/script.js
@@ -5,19 +5,4 @@ fetch("../include/footer.html")
 .then((response) => response.text())
 .then((data) => document.querySelector("#footer").innerHTML = data);
 
-const last = new Date(document.lastModified);
-const year = last.getFullYear();
-const month = last.getMonth() + 1;
-const date = last.getDate();
-let fixedMonth = month;
-let fixedDate = date;
-if (month < 10) {
-    fixedMonth = "0" + month;
-}
-if (date < 10) {
-    fixedDate = "0" + date;
-}
-const datetimeText = year + "-" + fixedMonth + "-" + fixedDate;
-const target = document.getElementById('last_modified');
-target.textContent = datetimeText;
-target.setAttribute("datetime", datetimeText);
+hljs.highlightAll();
