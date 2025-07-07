38797ccc70dc1e1d7424bb90b8614f01b3eea03a
lev635 <ryux635@gmail.com>
2025-07-08 00:27:02 +0900

    fmt

--- example-remark-plugin.mjs
+++
@@ -1,6 +0,0 @@
-export function exampleRemarkPlugin() {
-  // すべての remark および rehype プラグインは、別の関数を返します。
-  return function (tree, file) {
-    file.data.astro.frontmatter.customProperty = '生成されたプロパティ';
-  }
-}
